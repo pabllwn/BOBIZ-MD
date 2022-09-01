ac499898259b569005a44a64295f3dec375eaf2e
pabllwn <92005758+pabllwn@users.noreply.github.com>
2022-09-01 20:20:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,13 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './tmp' : process.env.DA
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
 module.exports = {
-    SESSION: process.env.BOBIZ_SESSION === undefined ? '' : process.env.BOBIZ_SESSION,
+    SESSION: process.env.BOBIZ_SESSION === undefined ? '' : process.env.Code_SESSION,
     URL_1NAME: process.env.URL_1NAME === undefined ? '🎃Follow me on Instagram🎃' : process.env.URL_1NAME,
-    URL_1LINK: process.env.URL_1LINK === undefined ? 'http://instagram.com/noureddine_ouafy' : process.env.URL_1LINK,
+    URL_1LINK: process.env.URL_1LINK === undefined ? 'http://instagram.com/' : process.env.URL_1LINK,
     URL_2NAME: process.env.URL_2NAME === undefined ? '✨Star on Github✨' : process.env.URL_2NAME,
     URL_2LINK: process.env.URL_2LINK === undefined ? 'https://github.com/sanuwaofficial' : process.env.URL_2LINK,
-    FOOTER: process.env.FOOTER === undefined ? 'BOBIZBOT-MD  instagram.com/noureddine_ouafy تابعني على الانستغرام ' : process.env.FOOTER,
-    CAPTION: process.env.CAPTION === undefined ? '🐶 𝙱𝙾𝙱𝙸𝚉 𝙱𝙾𝚃 𝙼𝙳 https://www.instagram.com/noureddine_ouafy  🐕' : process.env.CAPTION,
+    FOOTER: process.env.FOOTER === undefined ? 'BOBIZBOT-MD   تابعني على الانستغرام ' : process.env.FOOTER,
+    CAPTION: process.env.CAPTION === undefined ? 'skt' : process.env.CAPTION,
     ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
     ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://telegra.ph/file/201b5c02be1e336a29c60.jpg' : process.env.ALIVE_LOGO,
     SONG_DOWN: process.env.SONG_DOWN === undefined ? '*📥Downloading your song...*' : process.env.SONG_DOWN,
